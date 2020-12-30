117e90309298187297a7df1f5c5738426e1758ad
Aleksandar <aca801@gmail.com>
2020-12-30 23:40:25 +0100

    Add NotPassedPointException

---
+++ Exceptions/NotPassedPointException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ImageWorker.Exceptions
+{
+    public class NotPassedPointException : Exception
+    {
+        public NotPassedPointException()
+            : base("Please provide points for this action.")
+        {
+
+        }
+    }
+}
