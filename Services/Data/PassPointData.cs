b0049cfbebeab5ffe552beeaed2c2f5ebd9e3c21
Aleksandar <aca801@gmail.com>
2020-12-30 23:23:24 +0100

    Add implementation for points for the crop

---
+++ Services/Data/PassPointData.cs
@@ -0,0 +1,9 @@
+﻿using ImageWorker.Services.Interfaces;
+
+namespace ImageWorker.Services.Data
+{
+    public class PassPointData : IPassPointData
+    {
+        public (int PointA, int PointB, int PointC, int PointD) Points { get; set; } = (0, 0, 0, 0);
+    }
+}
---
+++ Services/Interfaces/IPassPointData.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageWorker.Services.Interfaces
+{
+    public interface IPassPointData
+    {
+        (int PointA, int PointB, int PointC, int PointD) Points { get; set; }
+    }
+}
